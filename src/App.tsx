fa1c0da095525bf3122990a9b3d5e444cb00d92d
ndelarans <nataliadelarans02@gmail.com>
2024-11-07 12:41:43 -0500

    update change

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 // src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './pages/Header'; // Asegúrate de que la ruta sea correcta
-import Home from './pages/Home'; // Importa el componente Home
-import Encuesta from './pages/Encuesta'; // Importa el componente Encuesta
-import Contactanos from './pages/Contactanos'; // Importa el componente Contactanos
+import Header from './pages/Header';
+import Home from './pages/Home';
+import Encuesta from './pages/Encuesta';
+import Contactanos from './pages/Contactanos';
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <Router basename="/Cuestionario"> {/* Establece el basename para las rutas */}
       <div>
         <Header />
         <main className="p-4">
           <Routes>
-            <Route path="/" element={<Home />} /> {/* Agrega Home como la ruta para el inicio */}
-            <Route path="/encuesta" element={<Encuesta />} /> {/* Usa el componente Encuesta aquí */}
-            <Route path="/contacto" element={<Contactanos />} /> {/* Ruta para el componente Contactanos */}
+            <Route path="/" element={<Home />} /> {/* Ruta para el inicio */}
+            <Route path="/encuesta" element={<Encuesta />} /> {/* Ruta para encuesta */}
+            <Route path="/contacto" element={<Contactanos />} /> {/* Ruta para contacto */}
           </Routes>
         </main>
       </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/', // Desarrollar sin la subcarpeta
+  base: '/Cuestionario/', // Asegúrate de que sea el nombre del repositorio
   build: {
     outDir: 'dist',
-  }
+  },
 });
